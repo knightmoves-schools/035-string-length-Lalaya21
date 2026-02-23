eb5bccf9bf88f99cff72c2541e76154f5cf92125
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-02-23 19:33:04 +0000

    done2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-let quoteLength = 
+let quoteLength = quote.length;
 let [twentySecondLetter] = quote21;
+let output = "Length" + quoteLength;
+output = output + 
 //Add code below
+document.getElementById('result').innerHTML= output;
