a9f17e3dac27734ea8f00109ea122a56d5469e9b
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-02-23 19:53:34 +0000

    ``

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-let quoteLength = quote.length;
-let twentySecondLetter = quote[21];
-let output = 'this is my string with ${qouteLength} and also [${twentySecondLetter}]';
+let quoteLength= quote.length;
+let twentySecondLetter= quote[21];
+let output= `this is my string with ${qouteLength} and also [${twentySecondLetter}]`;
 //Add code below
 document.getElementById('result').innerHTML = output;
